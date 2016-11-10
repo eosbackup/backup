d97e14d4e91de57b7b8c4c2b6d3cb0f42361b8e9
Kirill Zhukov <k_zhukov@step.ru>
2016-11-10 17:22:21 +0300

    test

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WORKDIR=/home/kzhukov/backups
+ls $WORKDIR
+
+cp /home/kzhukov/eos/db/eos.db $WORKDIR/eos_$(date +%d-%m-%Y).db
+
+
+cd $WORKDIR
+numOfFiles=$(ls *.db | wc -l)
+echo $numOfFiles
+
+while [[ numOfFiles -gt 5 ]]
+do
+	echo 'deleting old files'
+	oldest=$(ls -1t  *.db | tail -1 )
+	git rm $oldest
+	echo $oldest was deleted
+	numOfFiles=$(ls *.db | wc -l)	
+done
+	git add .
+	git commit -m 'backup '+$(date +%d-%m-%Y)
+	git push origin master
